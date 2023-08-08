d53c47372b12b7607fc581d2325a0aebef947bc7
Gaozj <48663858+ZjGaothu@users.noreply.github.com>
2023-08-08 19:08:31 +0800

    Update download_raw_data.sh

--- download_raw_data.sh
+++ download_raw_data.sh
@@ -37,7 +37,7 @@ function downloadRseq()
     grep -P "$CELL"     |
     awk  -F "\t"   	'
     {
-        print $1 "\t" $47 "\t" $45 "\t" $7
+        print $1 "\t" $48 "\t" $46 "\t" $7 "\t" $11
     }' |
     while read line; do
         let count+=1
@@ -46,8 +46,65 @@ function downloadRseq()
         furl=${item[1]}
         msum=${item[2]}
         exp=${item[3]}
+        cellname=${item[4]}
+        echo $furl $cellname $CELL
         fbed=$Rseq/$exp/$facc.tsv
         fmd5=$Rseq/$exp/$facc.md5
+        `
+        if [ $cellname == $CELL ]
+        then
+          for ((i = 0; i < 5; i++)); do
+            if [[ ! -e $fmd5 || \`cat $fmd5\` != $msum ]]; then
+                mkdir -p \`dirname $fbed\`
+                printf "[Downloading] %s\t%s\t%s\t%s\n" $facc $furl $msum $exp
+                curl  -o $fbed -L $furl -C - -s
+                if [  -e $fbed ]; then
+                    md5sum $fbed | cut -d ' ' -f 1 > $fmd5
+                fi
+            fi
+        done  
+        fi
+        ` 
+#         sleep 1s; while [ `ps -T | grep -P "\s+curl$" | wc -l` -ge 5 ]; do sleep 1s; done
+    echo "$count RNA-seq .tsv files download finished"
+    done
+}
+
+
+#download ChIP-seq data (.bam file)
+function downloadCseq()
+{
+    #downloading ChIP-seq data(TF+histone)
+    count=0
+    cat $mpath |
+    tail -n +2			|
+    grep -P "$gname"	|
+    grep ChIP-seq       |
+    grep bam            |
+    grep hg19 |
+    awk -F '\t' '$5=="alignments"' |
+    #remove control and histone marker (chip-seq)
+    #grep -v -P "control" |
+    #grep -v -P "\sH\d+"  |
+    grep -P "$CELL"		  |
+    grep -F -f "$DPATH/targets.txt" |
+    #sort -d	-k1,1	  |
+    awk  -F "\t"   	'
+    {
+        print $1 "\t" $48 "\t" $46 "\t" $7 "\t" $23 "\t" $11
+    }' |
+    while read line; do
+        let count+=1
+        item=(${line//\\t/})
+        facc=${item[0]}
+        furl=${item[1]}
+        msum=${item[2]}
+        exp=${item[3]}
+        target=${item[4]}
+        cellname=${item[5]}
+        fbed=$Cseq/$target/$exp/$facc.bam
+        fmd5=$Cseq/$target/$exp/$facc.md5
+        echo $furl >> ./ChIP_target.txt
         `
         for ((i = 0; i < 5; i++)); do
             if [[ ! -e $fmd5 || \`cat $fmd5\` != $msum ]]; then
@@ -60,58 +117,9 @@ function downloadRseq()
             fi
         done
         ` 
-        #sleep 1s; while [ `ps -T | grep -P "\s+curl$" | wc -l` -ge 5 ]; do sleep 1s; done
-    echo "$count RNA-seq .tsv files download finished"
-    done
-}
-
-
-#download ChIP-seq data (.bam file)
-function downloadCseq()
-{
-    #downloading ChIP-seq data(TF+histone)
-    count=0
-    cat  $mpath		|
-    tail -n +2			|
-    grep -P "$gname"	|
-    grep ChIP-seq       |
-    grep bam            |
-    awk -F '\t' '$5=="alignments"' |
-    #remove control and histone marker (chip-seq)
-    #grep -v -P "control" |
-    #grep -v -P "\sH\d+"  |
-    grep -P "$CELL"		  |
-    grep -F -f "$DPATH/targets.txt" |
-    #sort -d	-k1,1	  |
-    awk  -F "\t"   	'
-    {
-        print $1 "\t" $47 "\t" $45 "\t" $7 "\t" $22
-    }' |
-    while read line; do
-        let count+=1
-        item=(${line//\\t/})
-        facc=${item[0]}
-        furl=${item[1]}
-        msum=${item[2]}
-        exp=${item[3]}
-        target=${item[4]}
-        fbed=$Cseq/$target/$exp/$facc.bam
-        fmd5=$Cseq/$target/$exp/$facc.md5
-        `
-        for ((i = 0; i < 5; i++)); do
-            if [[ ! -e $fmd5 || \`cat $fmd5\` != $msum ]]; then
-                mkdir -p \`dirname $fbed\`
-                printf "[Downloading] %s\t%s\t%s\t%s\n" $facc $furl $msum $exp
-                curl  -o $fbed -L $furl -C - -s
-                if [  -e $fbed ]; then
-                    md5sum $fbed | cut -d ' ' -f 1 > $fmd5
-                fi
-            fi
+            #sleep 1s; while [ `ps -T | grep -P "\s+curl$" | wc -l` -ge 5 ]; do sleep 1s; done
+        echo "${count} ChIP-seq .bam files download finished"
         done
-        ` &  #backend (multi-process)
-        #sleep 1s; while [ `ps -T | grep -P "\s+curl$" | wc -l` -ge 5 ]; do sleep 1s; done
-    echo "${count} ChIP-seq .bam files download finished"
-    done
     #echo "${count} ChIP-seq .bam files download finished"
 }
 
@@ -125,12 +133,13 @@ tail -n +2			|
 grep -P "$gname"	|
 grep DNase-seq |
 grep bam |
+grep hg19 |
 awk -F '\t' '$5=="alignments"' |
 grep -P "$CELL"		  |
 #sort -d	-k1,1	  |
 awk  -F "\t"   	'
 {
-	print $1 "\t" $47 "\t" $45 "\t" $7
+	print $1 "\t" $48 "\t" $46 "\t" $7 "\t" $11
 }' |
 while read line; do
         let count+=1
@@ -139,21 +148,27 @@ while read line; do
 	furl=${item[1]}
 	msum=${item[2]}
 	exp=${item[3]}
+    cellname=${item[4]}
 	fbed=$Dseq/$exp/$facc.bam
 	fmd5=$Dseq/$exp/$facc.md5
-	`
-	for ((i = 0; i < 5; i++)); do
-		if [[ ! -e $fmd5 || \`cat $fmd5\` != $msum ]]; then
-			mkdir -p \`dirname $fbed\`
-			printf "[Downloading] %s\t%s\t%s\t%s\n" $facc $furl $msum $exp
-			curl  -o $fbed -L $furl -C - -s
-			if [  -e $fbed ]; then
-				md5sum $fbed | cut -d ' ' -f 1 > $fmd5
-			fi
-		fi
-	done
-        ` & 
-	#sleep 1s; while [ `ps -T | grep -P "\s+curl$" | wc -l` -ge 5 ]; do sleep 1s; done
+    echo $furl >> DNase_target1.txt
+    echo $fbed >> path_target1.txt
+    `
+    if [$cellname == $CELL]
+    then
+      for ((i = 0; i < 5; i++)); do
+        if [[ ! -e $fmd5 || \`cat $fmd5\` != $msum ]]; then
+            mkdir -p \`dirname $fbed\`
+            printf "[Downloading] %s\t%s\t%s\t%s\n" $facc $furl $msum $exp
+            curl  -o $fbed -L $furl -C - -s
+            if [  -e $fbed ]; then
+                md5sum $fbed | cut -d ' ' -f 1 > $fmd5
+            fi
+        fi
+    done  
+    fi
+    ` 
+# 	sleep 1s; while [ `ps -T | grep -P "\s+curl$" | wc -l` -ge 5 ]; do sleep 1s; done
 echo "${count} DNase-seq .bam files download finished."
 done
 #echo "${count} DNase-seq .bam files download finished."
@@ -183,5 +198,3 @@ while getopts "i:hrcd" Option;do
         ;;
     esac
 done
-
-
