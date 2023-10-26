1743e523e37b27e1f869508a620d7d94cc4fe231
Gaozj <48663858+ZjGaothu@users.noreply.github.com>
2023-10-26 14:04:23 +0800

    Update utils.py

--- model_hg38/utils.py
+++ model_hg38/utils.py
@@ -3,6 +3,7 @@ import os
 from pyfasta import Fasta
 import numpy as np
 import pandas as pd
+import time
 
 acgt2num = {'A': 0,
             'C': 1,
@@ -40,3 +41,31 @@ def load_weights(model,path):
     model = model.eval()
     model = model.cuda()
     return model
+
+def get_motif_score(tf_list, motif2tf, isContain, region_file, motifscan_file, save = True):
+    """parse the motifscan results by homer tool.
+    Args:
+        region_file: path to bed file that was used for motif scanning
+        motifscan_file: path to the motifscan file from Homer tool
+    """
+    regions = ['_'.join(item.split('\t')[:4]) for item in open(region_file).readlines()]
+    motifscore = np.empty((len(regions), len(tf_list)), dtype = 'float32')
+    count = 0
+    f_motifscan = open(motifscan_file,'r')
+    #skip header
+    line = f_motifscan.readline()
+    print(line)
+    line = f_motifscan.readline()
+    print(line)
+    while line != '':
+        motif = line.split('\t')[3]
+        if isContain[motif]:
+            region_id = line.split('\t')[0]
+            score = float(line.split('\t')[-1].rstrip())
+            for tf in motif2tf[motif]:
+                previous = motifscore[int(region_id)-1][tf_list.index(tf)]
+                motifscore[int(region_id)-1][tf_list.index(tf)] = max(score, previous)
+        line = f_motifscan.readline()
+        count += 1
+    f_motifscan.close()
+    np.save('motifscore.npy', motifscore)
